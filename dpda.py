7a8f2bdaaa2c5327335a1a883c4f37d6d6e5e5e0
Benjamin Baird <benjaminrbaird@gmail.com>
2022-04-19 18:42:07 -0400

    Fixed stupid bug. Jank implementation probs more

--- dpda.py
+++ dpda.py
@@ -5,17 +5,23 @@ class State:
         self.final = False
         self.transition_rules = dict()
 
-    #maps a state to a symbol. Can map multiple states to same symbol
-    def add_rule(self, reading_symbol, destination_state):
+    #maps a state to a symbol. Can map multiple states and stack data to same symbol
+    def add_rule(self, reading_symbol, transition_action):
         if reading_symbol in self.transition_rules:
-            self.transition_rules[reading_symbol].append(destination_state)
+            self.transition_rules[reading_symbol].append(transition_action)
         else:
-            self.transition_rules[reading_symbol] = [destination_state]
+            self.transition_rules[reading_symbol] = [transition_action]
 
     #return next symbol if transition rule exists
-    def read_symbol(self, reading_symbol):
+    def read_symbol(self, reading_symbol, stack_symbol="@"):
         if reading_symbol in self.transition_rules:
-            return self.transition_rules[reading_symbol]
+            transition_actions = self.transition_rules[reading_symbol]
+            result = []
+            for ta in transition_actions:
+                if (ta["stack_read"] == "@" or ta["stack_read"] == stack_symbol):
+                    result.append(ta)
+            print(result)
+            if result: return result
         return [None]
 
     def set_final(self, isFinal):
@@ -41,9 +47,10 @@ class NFA:
         self.start = None
         self.accepts = False
         self.new()
+        self.stack = list()
 
     #creates a new NFA as specified by input NFA file.
-    def new(self, nfa_file_name='nfa.txt'):
+    def new(self, nfa_file_name='dpda.txt'):
         self.states = {}
         self.alphabet = set()
         self.start = None
@@ -56,6 +63,8 @@ class NFA:
             self.alphabet.add('@')
             letters = nfa_file.readline().rstrip().split(',')
             for l in letters: self.alphabet.add(l)
+            #ignore stack alphabet
+            stack_alphabet = nfa_file.readline().rstrip()
             #set start state
             start_state = nfa_file.readline().rstrip()
             self.states[start_state].set_start(True)
@@ -67,7 +76,12 @@ class NFA:
             line = nfa_file.readline()
             while line:
                 rule = line.rstrip().split(',')
-                self.states[rule[0]].add_rule(rule[1], self.states[rule[2]])
+                transition_action = {"symbol_read" : rule[1], 
+                                     "stack_read" : rule[2], 
+                                     "next_state" : self.states[rule[3]],
+                                     "stack_push" : rule[4]}
+                print(transition_action)
+                self.states[rule[0]].add_rule(rule[1], transition_action)
                 line = nfa_file.readline()
             nfa_file.close()
 
@@ -83,7 +97,7 @@ class NFA:
                 for string in input_file:
                     self.accepts = False
                     #sim function
-                    self.simulate(self.start, string.rstrip())
+                    self.simulate(self.start, string.rstrip(), self.stack)
                     if self.accepts:
                         out.write('accept\n')
                     else:
@@ -93,15 +107,42 @@ class NFA:
 
 
     #recursively simulate string on NFA
-    def simulate(self, current, string, empty_cycle=set()):
+    def simulate(self, current, string, stack, stack_push="@", empty_cycle=set(), iter=0):
         #kills path if no transition rule for previous symbol or path is @ cycle.
         if current == None or current in empty_cycle: return
-
+        
+        
+        if stack_push != "@": stack.append(stack_push)
         #if in valid state and not in cycle try and read @. Keep track of states visited to prevent cycles.
-        empty_next = current.read_symbol('@')
+        empty_string_stack = stack.copy()
+        stack_top = ""
+        if stack: stack_top = empty_string_stack.pop()
+        empty_next = current.read_symbol('@', stack_top)
         new_empty_cycle = empty_cycle.copy()
         new_empty_cycle.add(current)
-        for s in empty_next: self.simulate(s, string, new_empty_cycle)
+        print("stack: ", stack)
+        print(iter)
+        iter += 1
+        for s in empty_next:
+            if s and s["stack_read"] == "@":
+                print("@, @ stack sim :", s)
+                self.simulate(
+                    s["next_state"], 
+                    string, 
+                    stack.copy(), 
+                    s["stack_push"], 
+                    new_empty_cycle,
+                    iter)
+            else:
+                if s: 
+                    print("@, r stack sim :", s)
+                    self.simulate(
+                    s["next_state"], 
+                    string, 
+                    empty_string_stack.copy(), 
+                    s["stack_push"], 
+                    new_empty_cycle,
+                    iter)
 
         #if input string is empty and current state is accept state set accepts var to true
         if not string:
@@ -109,8 +150,25 @@ class NFA:
             return
 
         #if input string not empty read leading character and check if any path from it is valid
-        next_states = current.read_symbol(string[0])
-        for s in next_states: self.simulate(s, string[1:])
+        next_stack = stack.copy()
+        stack_top = ""
+        if stack: stack_top = next_stack.pop()
+        next_states = current.read_symbol(string[0], stack_top)
+        for s in next_states: 
+            if s and s["stack_read"] == "@":
+                print("r, @ stack sim :", s)
+                self.simulate(
+                    s["next_state"], 
+                    string[1:], 
+                    stack.copy(), 
+                    s["stack_push"])
+            elif s: 
+                print("r, r stack sim :", s)
+                self.simulate(
+                    s["next_state"], 
+                    string[1:], 
+                    next_stack.copy(), 
+                    s["stack_push"])
 
 
     def __str__(self) -> str:
